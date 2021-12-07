0f066c40425e1308aca4eae01ea519973cba473c
Ligia Santos <ligia.baptista.santos@gmail.com>
2021-12-07 12:47:41 +0000

    Initial commit.
    (cherry picked from commit cb95bb3a932f835d6cc8d4e4d94b38d8a5ccd96d)

---
+++ src/Application.java
@@ -0,0 +1,9 @@
+import exercises.Homework;
+
+public class Application {
+
+    public static void main(String[] args) {
+        Homework homework = new Homework();
+        homework.printHello();
+    }
+}
