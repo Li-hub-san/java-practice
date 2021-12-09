8381ebec415c5cf6b5b847b2b162693a4e7b01b3
Ligia Santos <ligia.baptista.santos@gmail.com>
2021-12-09 20:43:15 +0000

    Did exercise 8 (schoolWork)

--- src/exercises/school/Exercise8.java
+++ src/exercises/school/Exercise8.java
@@ -1,5 +1,19 @@
 package exercises.school;
 
+import java.util.Scanner;
+
 public class Exercise8 {
 
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		System.out.print("Input A number: ");
+		int numberA = sc.nextInt();
+
+		System.out.print("Input B number: ");
+		int numberB = sc.nextInt();
+
+		System.out.println("Number A = " + numberB + "\n" + "Number B = " + numberA);
+		sc.close();
+	}
 }
