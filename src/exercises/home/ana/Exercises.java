26ba8d6911fc7a2ed718716f7356e23e08e83a08
Li-hub-san <ligia.baptista.santos@gmail.com>
2022-01-11 14:18:24 +0000

    Altered ana's 3rd exercise.

--- src/exercises/home/ana/Exercises.java
+++ src/exercises/home/ana/Exercises.java
@@ -5,108 +5,111 @@ import java.util.List;
 import java.util.Scanner;
 
 public class Exercises {
-  private final Scanner sc = new Scanner(System.in);
+    private final Scanner sc = new Scanner(System.in);
 
-  public void exercise1And2() {
-    int numberOfFriends = requestNumberOfFriends();
-    List<Friend> friends = requestFriends(numberOfFriends);
-    Friend oldest = getOldestFriend(friends);
-    Friend youngest = getYoungestFriend(friends);
-    System.out.println();
-    System.out.println("Oldest friend: " + oldest);
-    System.out.println("Youngest friend: " + youngest);
-  }
-
-  private int requestNumberOfFriends() {
-    System.out.print("Input how many close friends you've got: ");
-    return sc.nextInt();
-  }
-
-  private String requestFriendName(int friendIndex) {
-    System.out.print("Input friend " + (friendIndex + 1) + " name: ");
-    String name = sc.next();
-    return name.substring(0, 1).toUpperCase() + name.substring(1).toLowerCase();
-  }
-
-  private int requestFriendAge(String name) {
-    System.out.print("How old is " + name + "? ");
-    return sc.nextInt();
-  }
-
-  private int requestMonthOfBirth(String name) {
-    System.out.print("Input " + name + "'s birth month number: ");
-    return sc.nextInt();
-  }
-
-  private List<Friend> requestFriends(int numberOfFriends) {
-    List<Friend> friends = new ArrayList<>();
-
-    for (int i = 0; i < numberOfFriends; i++) {
-      String name = requestFriendName(i);
-      int age = requestFriendAge(name);
-      int month = requestMonthOfBirth(name);
-
-      Friend friend = new Friend(name, age, month);
-      friends.add(friend);
+    public void exercise1And2() {
+        int numberOfFriends = requestNumberOfFriends();
+        List<Friend> friends = requestFriends(numberOfFriends);
+        Friend oldest = getOldestFriend(friends);
+        Friend youngest = getYoungestFriend(friends);
+        System.out.println();
+        System.out.println("Oldest friend: " + oldest);
+        System.out.println("Youngest friend: " + youngest);
     }
 
-    return friends;
-  }
-
-  private Friend getOldestFriend(List<Friend> friends) {
-    return friends.stream()
-        .reduce(friends.get(0), (friend, friend2) -> {
-          if (friend.getAge() == friend2.getAge()) {
-            return friend.getMonth() < friend2.getMonth() ? friend : friend2;
-          }
-          return friend.getAge() > friend2.getAge() ? friend : friend2;
-        });
-  }
-
-  private Friend getYoungestFriend(List<Friend> friends) {
-    return friends.stream()
-        .reduce(friends.get(0), (friend, friend2) -> {
-          if (friend.getAge() == friend2.getAge()) {
-            return friend.getMonth() > friend2.getMonth() ? friend : friend2;
-          }
-          return friend.getAge() < friend2.getAge() ? friend : friend2;
-        });
-  }
-
-
-  public void exercise3() {
-    int numberOfClassMates = requestNumberOfClassmates();
-    List<Colleague> colleagues = requestAges(numberOfClassMates);
-    double averageAge = calculateAverageAge(colleagues);
-    System.out.println("Class average age: " + averageAge);
-  }
-
-
-  private int requestNumberOfClassmates() {
-    System.out.print("Input how many students there are on your class: ");
-    return sc.nextInt();
-  }
-
-  private List<Colleague> requestAges(int numberOfClassMates) {
-    List<Colleague> colleagues = new ArrayList<>();
-
-    for (int i = 0; i < numberOfClassMates; i++) {
-      System.out.print("Input the age of student " + (i + 1) + ": ");
-      int currentAge = sc.nextInt();
-
-      Colleague colleague = new Colleague(currentAge);
-      colleagues.add(colleague);
+    private int requestNumberOfFriends() {
+        System.out.print("Input how many close friends you've got: ");
+        return sc.nextInt();
     }
 
-    return colleagues;
-  }
+    private String requestFriendName(int friendIndex) {
+        System.out.print("Input friend " + (friendIndex + 1) + " name: ");
+        String name = sc.next();
+        return name.substring(0, 1).toUpperCase() + name.substring(1).toLowerCase();
+    }
 
-  private double calculateAverageAge(List<Colleague> colleagues) {
-    int totalAgeSum = colleagues.stream()
-        .map(Colleague::getAge)
-        .reduce(0, Integer::sum);
+    private int requestFriendAge(String name) {
+        System.out.print("How old is " + name + "? ");
+        return sc.nextInt();
+    }
 
-    return (double) totalAgeSum / colleagues.size();
-  }
+    private int requestMonthOfBirth(String name) {
+        System.out.print("Input " + name + "'s birth month number: ");
+        return sc.nextInt();
+    }
+
+    private List<Friend> requestFriends(int numberOfFriends) {
+        List<Friend> friends = new ArrayList<>();
+
+        for (int i = 0; i < numberOfFriends; i++) {
+            String name = requestFriendName(i);
+            int age = requestFriendAge(name);
+            int month = requestMonthOfBirth(name);
+
+            Friend friend = new Friend(name, age, month);
+            friends.add(friend);
+        }
+
+        return friends;
+    }
+
+    private Friend getOldestFriend(List<Friend> friends) {
+        return friends.stream()
+                .reduce(friends.get(0), (friend, friend2) -> {
+                    if (friend.getAge() == friend2.getAge()) {
+                        return friend.getMonth() < friend2.getMonth() ? friend : friend2;
+                    }
+                    return friend.getAge() > friend2.getAge() ? friend : friend2;
+                });
+    }
+
+    private Friend getYoungestFriend(List<Friend> friends) {
+        return friends.stream()
+                .reduce(friends.get(0), (friend, friend2) -> {
+                    if (friend.getAge() == friend2.getAge()) {
+                        return friend.getMonth() > friend2.getMonth() ? friend : friend2;
+                    }
+                    return friend.getAge() < friend2.getAge() ? friend : friend2;
+                });
+    }
+
+
+    public void exercise3() {
+        int numberOfClassMates = requestNumberOfClassmates();
+        List<Integer> ages = requestAges(numberOfClassMates);
+        double averageAge = calculateAverageAge(ages);
+        System.out.println("Class average age: " + averageAge);
+    }
+
+
+    private int requestNumberOfClassmates() {
+        System.out.print("Input how many students there are on your class: ");
+        return sc.nextInt();
+    }
+
+    private List<Integer> requestAges(int numberOfClassMates) {
+        List<Integer> ages = new ArrayList<>();
+
+        for (int i = 0; i < numberOfClassMates; i++) {
+            System.out.print("Input the age of student " + (i + 1) + ": ");
+            int currentAge = sc.nextInt();
+
+
+            ages.add(currentAge);
+        }
+
+        return ages;
+    }
+
+    private double calculateAverageAge(List<Integer> ages) {
+        double totalAgesSum = 0;
+        for (int currentAge : ages) {
+            totalAgesSum += currentAge;
+        }
+
+        return totalAgesSum / ages.size();
+    }
 
 }
+
+
